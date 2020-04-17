1f316582a6dabe3fefcc5a50a6e18eddd834c8a2
mihiro <f.mihiroxx@gmail.com>
2020-04-17 20:22:32 +0900

    Make remember method to SessionsHelper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,12 @@ module SessionsHelper
     def log_in(user)
         session[:user_id] = user.id
     end
+    
+    def remember(user)
+        user.remember
+        cookies.permanent.signed[:user_id] = user.id
+        cookies.parmanent[:remember_token] = user.remember_token
+    end
 
     def current_user
         if session[:user_id]
