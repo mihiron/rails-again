4879c17ad90cebb3518d11c7462f54c041893cd1
mihiro <f.mihiroxx@gmail.com>
2020-04-19 15:45:26 +0900

    Add admin column to UserDB

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 User.create!(name: "Example User", email:"example@railstutorial.org",
-            password:"foobar", password_confirmation: "foobar")
+            password:"foobar", password_confirmation: "foobar",
+            admin: true)
 
 99.times do |n|
     name = Faker::Name.name
