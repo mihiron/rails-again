740afbaebb90e5945773c6b02f6fd8f7676a4d86
mihiro <f.mihiroxx@gmail.com>
2020-04-20 13:17:47 +0900

    Fix bug

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,7 +2,7 @@ class MicropostsController < ApplicationController
     before_action :logged_in_user, only: [:create, :destroy]
 
     def create
-        @micropost = current_user.micropost.build(micropost_params)
+        @micropost = current_user.microposts.build(micropost_params)
         if @micropost.save
             flash[:success] = "Micropost created!"
             redirect_to root_url
