9f4f7c33bd39c611121bb8fcb72e93e27ae6c112
mihiro <f.mihiroxx@gmail.com>
2020-04-20 13:29:37 +0900

    Add micropost destroy action & delete view

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
     before_action :logged_in_user, only: [:create, :destroy]
+    before_action :corrent_user, only: :destroy
 
     def create
         @micropost = current_user.microposts.build(micropost_params)
@@ -13,10 +14,18 @@ class MicropostsController < ApplicationController
     end
 
     def destroy
+        @micropost.destroy
+        flash[:success] = "Micropost deleted"
+        redirect_to request.referrer || root_url
     end
 
     private
         def micropost_params
             params.require(:micropost).permit(:content)
+        end
+
+        def current_user
+            @micropost = current_user.microposts.find_by(id: params[:id])
+            redirect_to root_url if @micropost.nil?
         end
 end
